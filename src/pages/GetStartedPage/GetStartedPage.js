febc8ba83147a8d0aab3f1cf54526ca90d6d9e16
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-12-19 15:45:14 +0000

    Feat: implement header page

---
+++ src/pages/GetStartedPage/GetStartedPage.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "./GetStartedPage.scss";
+import imgHero from "../../assets/images/Horror1.png";
+
+const GetStartedPage = () => {
+  return (
+    <div className="main">
+      <div className="get-started-page">
+        <h1>Get Started Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default GetStartedPage;
