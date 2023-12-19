a9164aa5e105c08c218407930daecf58bb43df9e
Olusanya Jegede <olusanyajegede1@gmail.com>
2023-12-19 16:00:35 +0000

    Feat: header component style and structure

--- src/pages/GetStartedPage/GetStartedPage.js
+++ src/pages/GetStartedPage/GetStartedPage.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import "./GetStartedPage.scss";
 import imgHero from "../../assets/images/Horror1.png";
+import Header from "../../components/Header/Header";
 
 const GetStartedPage = () => {
   return (
     <div className="main">
       <div className="get-started-page">
+        <Header />
         <h1>Get Started Page</h1>
       </div>
     </div>
